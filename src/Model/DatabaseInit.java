7cd0850046cbeeae04f9f28cfa27e1c884cd0383
Jesper Bruun <jeha13ad@student.cbs.dk>
2014-11-09 13:01:58 +0100

    Databaseinit og gui

---
+++ src/Model/DatabaseInit.java
@@ -0,0 +1,32 @@
+package Model;
+
+import Config.Config;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Created by jesperbruun on 09/11/14.
+ */
+public class DatabaseInit extends Model {
+
+    public static void init() {
+
+        try {
+            if (!doesDatabaseExist())
+            {
+                readfromSqlFile(Config.getResPath() + "createDBscript.sql");
+            }
+        }
+        catch (SQLException e){
+            System.err.println(e.getStackTrace());
+        }
+        catch (IOException e) {
+            System.err.println(e.getStackTrace());
+        }
+
+
+    }
+
+
+}
---
+++ src/View/LoginInfo.java
@@ -0,0 +1,9 @@
+package View;
+
+public class LoginInfo {
+	public void info(){
+		String username = "hej";
+		String password = "nej";
+	}
+
+}
